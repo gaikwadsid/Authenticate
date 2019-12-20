2cf4e88b3f143f2a29c11c7db7c3353b9aa406d1
gaikwadsid <sidgaikwad853@gmail.com>
2019-12-20 02:56:16 +0000

    Completed New User Interface without CSS

--- Authenticate.py
+++ Authenticate.py
@@ -10,26 +10,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
- #   return render_template("converter.html")
-
-if __name__ == '__main__':
-    app.run()
+    return render_template("Options.html")
 
 
 # Run the program
 # Calls display_welcome_screen() and authenticate()
+'''
+    mi = int(request.values.get("mi"))
+    km = mi * 1.62137
+    km = round(km, 5)
+    return render_template("result.html", km=km, mi=mi)
+'''
+@app.route("/home")
 def main():
-    option = display_welcome_screen()
+    option = request.values.get("opt")
     if (option == 1):
         authenticate()
-    if (option == 2):
-        new_user()
+    if (option == "2"):
+        return render_template("New_User.html")
     if (option == 3):
         program_exit()
 
 
 # Displays program title
+"""
 def display_welcome_screen():
     print("*********************")
     print("Secure System Access")
@@ -38,7 +42,7 @@ def display_welcome_screen():
     print("Please slect from the following options: ")
     choice = int(input("1. Login\n2. New User Account\n3. Quit\n>>>"))
     return choice
-
+    """
 
 # checks the password for the given username
 def database(username, password):
@@ -61,7 +65,7 @@ def authenticate():
     n = 3
     while n > 0:
         password = input('Enter your password: ')
-        if (database(username, password) == True):
+        if database(username, password) == True:
             logged_in()
             break
         else:
@@ -74,24 +78,24 @@ def authenticate():
 
 
 # Adds a new username and password to the dictionary
+@app.route("/new_user")
 def new_user():
-    username = input('Enter username: ')
-    password = input('Enter password: ')
+    username = request.values.get("user")
+    password = request.values.get("pass")
     users = dict()
     with open("database.json", "r") as f:
         users = json.load(f)
         n = 1
         while n > 0:
-            if (str(username in users) == 'True'):
-                print("This username is taken.\nPlease pick a new one.")
-                username = input('Enter username: ')
+            if str(username in users) == 'True':
+                return render_template("try_again.html", user=username)
             else:
                 n = 0
         users[username] = password
         with open("database.json", "w") as f:
             users = json.dump(users, f, indent=2)
-        print("**Account successfully created**")
-        display_welcome_screen()
+        return render_template("created.html", user=username)
+
 
 
 # Exits the program if the user wants to
@@ -108,4 +112,7 @@ def logged_in():
     print("Welcome to the top secret system")
 
 
-main()
+if __name__ == '__main__':
+    app.run()
+
+
